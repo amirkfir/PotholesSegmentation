fe92b6ff5eddffc8e9d53cd64f2bd974e9ff6dc1
moskitoo <tomek_niedzi@o2.pl>
2024-11-16 23:11:40 +0100

    cleaned code

--- Python_Files/classification_manager.py
+++ Python_Files/classification_manager.py
@@ -51,7 +51,7 @@ def get_classification_results(model, dataloader, device):
             # print(f"predicted: {predicted}")
             # print(f"label: {label}")
 
-            result_pair = [predicted, label, path]
+            result = [predicted, label, path]
 
             if predicted != label:
                 scores["wrong"] += 1
@@ -59,58 +59,26 @@ def get_classification_results(model, dataloader, device):
                 scores["correct"] += 1
             scores["total"] += 1 
 
-            results.append(result_pair)
+            results.append(result)
 
-            if saved_images_count < max_images_to_save:
-                img = image.squeeze().cpu().detach().numpy()
-                img = np.transpose(img, (1, 2, 0))
+            # if saved_images_count < max_images_to_save:
+            #     print(result)
 
-                plt.figure(figsize=(6, 6))
-                plt.imshow(img)
-                plt.title(f'True Label: {label}, Predicted: {predicted}')
-                plt.axis('off')
+            #     img = image.squeeze().cpu().detach().numpy()
+            #     img = np.transpose(img, (1, 2, 0))
 
-                output_path = os.path.join(output_dir, f"image_{saved_images_count + 1}.png")
-                plt.savefig(output_path, bbox_inches='tight')
-                plt.close()
+            #     plt.figure(figsize=(6, 6))
+            #     plt.imshow(img)
+            #     plt.title(f'True Label: {label}, Predicted: {predicted}')
+            #     plt.axis('off')
 
-                saved_images_count += 1
+            #     output_path = os.path.join(output_dir, f"image_{saved_images_count + 1}.png")
+            #     plt.savefig(output_path, bbox_inches='tight')
+            #     plt.close()
+
+            #     saved_images_count += 1
 
     scores["ratio"] = scores["correct"] / scores["total"]
     print(scores)
 
     return results
-            # Check if prediction is incorrect
-            # if predicted != label:
-            #     # Get the score for the predicted class
-            #     score = output[0, predicted]
-            #     # Backward pass to calculate gradients
-            #     model.zero_grad()
-            #     score.backward()
-            #     # Get the saliency map by computing the maximum absolute gradient across color channels
-            #     saliency, _ = torch.max(image.grad.data.abs(), dim=1)
-            #     saliency = saliency.squeeze().cpu().numpy()
-            #     # Unnormalize the image for visualization
-            #     img = image.squeeze().cpu().detach().numpy()
-            #     img = np.transpose(img, (1, 2, 0))  # Convert from CxHxW to HxWxC
-            #     mean = np.array([0.5244, 0.4443, 0.3621])
-            #     std = np.array([0.2679, 0.2620, 0.2733])
-            #     img = std * img + mean  # Unnormalize
-            #     img = np.clip(img, 0, 1)
-            #     # Map class indices to class names
-            #     class_names = ['Hotdog', 'Not Hotdog']
-            #     true_class_name = class_names[label.item()]
-            #     predicted_class_name = class_names[predicted.item()]
-                # Plot the original image and its saliency map
-                # plt.figure(figsize=(13, 7))
-                # plt.subplot(1, 2, 1)
-                # plt.imshow(img)
-                # plt.title(f'Original Image\nTrue Label: {true_class_name}', fontsize=16)
-                # plt.axis('off')
-                # plt.subplot(1, 2, 2)
-                # plt.imshow(saliency, cmap='hot')
-                # plt.title(f'Saliency Map\nPrediction: {predicted_class_name} (Incorrect)', fontsize=16)
-                # plt.axis('off')
-                # plt.tight_layout(pad=3.0)  # Increase padding for more margin
-                # plt.show()
-                #return  # Exit after displaying the first misclassified image
