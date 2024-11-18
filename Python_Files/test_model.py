071da97226abafb29d450755caed35e73380ae97
moskitoo <tomek_niedzi@o2.pl>
2024-11-18 15:36:43 +0100

    results generated for unbiased porposals test set

--- Python_Files/test_model.py
+++ Python_Files/test_model.py
@@ -18,10 +18,13 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 image_resize = 128
 batch_size = 50
 
+dataset_name = 'Proposals_test_unbiased'
+# data_path = f'../data/Potholes/{dataset_name}/'
+
 object_trainset, object_testset, object_testset_extended, object_train_loader, object_test_loader, object_test_loader_extended = load_and_transform_objects(
                                                                                                   batch_size=batch_size,
                                                                                                   image_resize=image_resize,
-                                                                                                  data_path = '../data/Potholes/Proposals_test/',
+                                                                                                  data_path = f'../data/Potholes/{dataset_name}/',
                                                                                                   only_test=True)
 
 # create model instance
@@ -34,7 +37,7 @@ model.load_state_dict(torch.load('rcnn_model.pth'))
 
 results = get_classification_results(model, object_test_loader_extended, device)
 
-with open("test_results.pkl", "wb") as f:
+with open(f"{dataset_name}_results.pkl", "wb") as f:
     pickle.dump(results, f)
 
 # print(results)
